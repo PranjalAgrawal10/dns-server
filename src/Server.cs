a40ff35c53654792a8c1cafac7f3eb78b8d9b112
PranjalAgrawal <pranjal605@gmail.com>
2024-09-25 23:53:21 +0530

    Write header section

--- src/Server.cs
+++ src/Server.cs
@@ -1,33 +1,53 @@
-using System;
+using System.Buffers.Binary;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
-// // Resolve UDP address
 IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
 int port = 2053;
+
 IPEndPoint udpEndPoint = new IPEndPoint(ipAddress, port);
 
 // Create UDP socket
 UdpClient udpClient = new UdpClient(udpEndPoint);
 
+
 while (true)
 {
-    // Receive data
-    IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Any, 0);
-    byte[] receivedData = udpClient.Receive(ref sourceEndPoint);
-    string receivedString = Encoding.ASCII.GetString(receivedData);
-
-    Console.WriteLine($"Received {receivedData.Length} bytes from {sourceEndPoint}: {receivedString}");
-
-    // Create an empty response
-    byte[] response = Encoding.ASCII.GetBytes("");
-
-    // Send response
-    udpClient.Send(response, response.Length, sourceEndPoint);
+  // Receive data
+  IPEndPoint sourceEndPoint = new IPEndPoint(IPAddress.Any, 0);
+  var recResult = await udpClient.ReceiveAsync();
+  var responseHeader = new DnsHeader();
+  responseHeader.Flags |= 0x8000;
+  var memory = new Memory<byte>(new byte[12]);
+  responseHeader.Write(memory.Span);
+  await udpClient.SendAsync(memory, recResult.RemoteEndPoint);
 }
 
+public class DnsHeader
+{
+  public ushort TransactionId { get; set; } = 1234;
+
+  public ushort Flags { get; set; }
+  public ushort QuestionCount { get; set; }
+  public ushort AnswerRecordCount { get; set; }
+  public ushort AuthorityRecordCount { get; set; }
+  public ushort AdditionalRecordCount { get; set; }
+
+  public void Write(Span<byte> output)
+  {
+    if (output.Length < 12)
+    {
+      throw new ArgumentException("output too short");
+    }
+
+    BinaryPrimitives.WriteUInt16BigEndian(output, TransactionId);
+    BinaryPrimitives.WriteUInt16BigEndian(output[2..], Flags);
+    BinaryPrimitives.WriteUInt16BigEndian(output[4..], QuestionCount);
+    BinaryPrimitives.WriteUInt16BigEndian(output[6..], AnswerRecordCount);
+    BinaryPrimitives.WriteUInt16BigEndian(output[8..], AuthorityRecordCount);
+    BinaryPrimitives.WriteUInt16BigEndian(output[10..], AdditionalRecordCount);
+  }
+}
